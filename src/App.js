ff51fe1312e205daa21c5f3cccb267884a93c5b5
Dani Pardo <danielp.5@hotmail.com>
2022-07-13 09:58:48 +0200

    fins al random (falta)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from 'react';
+import ContactData from './contacts.json'
+import {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const contacts = ContactData.slice(0,5)
+
+  const getRandomContact = () =>{
+    const random= Math.floor(Math.random*ContactData.slice(5).length)
+    const newContact= ContactData.slice(5)[random]
+    return newContact
+  }
+
+  return (<div className="App">
+
+      <h1>IronContacts</h1>
+<button onClick={getRandomContact}>Add a random contact</button>
+      <table class="table">
+  <thead>
+    <tr>
+      <td>Picture</td> 
+      <td>Name</td>
+      <td>Popularity</td>
+      <td>Won Oscar</td>
+      <td>Won Emmy</td>
+    </tr>
+  </thead>
+  <tbody>
+    {contacts.map((contact)=>{
+      return(
+    
+      <tr className="famos">
+        <td><img src={contact.pictureUrl} alt={contact.name} width={100} height={150}/></td>
+        <td>{contact.name}</td>
+        <td>{Math.round(contact.popularity)}</td>
+        <td>{(contact.wonOscar)&& "🏆"}</td>
+        <td>{(contact.wonEmmy)&& "🏆"}</td>
+      </tr>
+    )
+    })}
+    <tr>
+    <td><img src={getRandomContact.pictureUrl} alt={getRandomContact.name} width={100} height={150}/></td>
+        <td>{getRandomContact.name}</td>
+        <td>{Math.round(getRandomContact.popularity)}</td>
+        <td>{(getRandomContact.wonOscar)&& "🏆"}</td>
+        <td>{(getRandomContact.wonEmmy)&& "🏆"}</td>
+    </tr>
+    </tbody>
+  </table>
+  </div>)
+
 }
+
+
+
+
 
 export default App;
